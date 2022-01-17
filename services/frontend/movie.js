e5b7e34c8f2706ee63224ed5a64ea3ff34ee92d6
JeongPope <nullptr-@kakao.com>
2022-01-17 22:41:42 +0900

    update: frontend

---
+++ services/frontend/movie.js
@@ -0,0 +1,30 @@
+getMovie()
+
+function getMovie() {
+    let url = 'http://localhost:8880/api/movie/'
+    let val = localStorage.getItem('classlion')
+    url += val
+
+    fetch(url)
+        .then(function (response) {
+            response.json().then(function (json) {
+                var movie = json
+                console.log(movie)
+
+                document.getElementsByClassName("container")[0].innerHTML +=
+                    "<img src=\"assets/" + movie.image + "\" " +
+                    "alt=\"" + movie.alt + "\" class=\"image\">";
+
+                document.getElementsByClassName("desc")[0].innerHTML +=
+                    "<center>" +
+                    "<h1>" + movie.title + "</h1>" +
+                    "<p1>" + movie.desc + "</p1>" +
+                    "<br><br>" +
+                    "<p3> JEONG JE HO </p3>" +
+                    "</center>";
+            })
+        })
+        .catch(function (error) {
+            console.log("Error: " + error);
+        });
+}
--- services/frontend/script.js
+++ services/frontend/script.js
@@ -1,6 +1,37 @@
 
 //alert("Please Use Desktop View For Better Expeience")
 filterSelection("all")
+getMovies()
+
+function getMovies() {
+  fetch('http://localhost:8880/api/movies')
+    .then(function (response) {
+      response.json().then(function (json) {
+        for (i = 0; i < json.length; i++) {
+          var movie = json[i]
+
+          document.getElementsByClassName("cards")[0].innerHTML +=
+            "<li>" +
+            "<a href=\"movie.html\">" +
+            "<div class=\"row\">" +
+            "<div class=\"column nature show\">" +
+            "<div class=\"content\" id=" + (i + 1) + "\" onclick=\"setMovie(" + (i + 1) + ")\">" +
+            "<img src=\"assets/" + movie.image + "\"" +
+            "alt=\"" + movie.alt + "\" style=\"width:100%\">" +
+            "<h4>" + movie.title + "</h4>" +
+            "</div>" +
+            "</div>" +
+            "</div>" +
+            "</a>" +
+            "</li>";
+        }
+      })
+    })
+    .catch(function (error) {
+      console.log("Error: " + error);
+    });
+}
+
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
@@ -33,13 +64,17 @@ function w3RemoveClass(element, name) {
 }
 
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+// // Add active class to the current button (highlight it)
+// var btnContainer = document.getElementById("myBtnContainer");
+// var btns = btnContainer.getElementsByClassName("btn");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
+
+function setMovie(id) {
+  localStorage.setItem('classlion', id)
 }
